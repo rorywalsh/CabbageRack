c4dfce15fb0589ed42a900508cc656fbef7e1499
rorywalsh <rorywalsh@ear.ie>
2019-09-05 19:42:54 +0100

    adding windows CI build

---
+++ buildWin.ps1
@@ -0,0 +1,13 @@
+dir  
+cd c:/
+Invoke-WebRequest -Uri "https://github.com/rorywalsh/cabbage/releases/download/v2.0.00/csound-windows_x64-6.13.0.zip" -OutFile "C:\csound-windows_x64-6.12.0.zip" 
+7z.exe x csound-windows_x64-6.12.0.zip -o"C:/Program Files"
+cd "C:/Program Files/Csound6_x64"
+dir
+
+
+cd D:/a/1/
+
+ls
+
+$env:Path += ";C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin"
