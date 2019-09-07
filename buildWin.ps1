814a974c62f35eab68a3b220cd8c32ca0a6899de
rorywalsh <rorywalsh@ear.ie>
2019-09-07 07:06:16 +0100

    updating

--- buildWin.ps1
+++ buildWin.ps1
@@ -13,7 +13,7 @@ Get-ChildItem -Path "D:/a/1" | Copy-Item -Destination "D:/a/Rack-SDK/CabbageRack
 cd D:/a/Rack-SDK
 dir
 
-C:\tools\msys64\usr\bin\bash.exe -lc "cd /d/a/Rack-SDK/CabbageRack;ls;"
+C:\tools\msys64\usr\bin\bash.exe -lc "cd /d;ls;"
 
 # C:\tools\msys64\usr\bin\bash.exe -lc "cd /d/a/Rack-SDK/CabbageRack;echo ++++++++++++++++++++++;ls"
 
