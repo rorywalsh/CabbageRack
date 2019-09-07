ecc38f921916a195dfa3862744bf8e2d7f695c84
rorywalsh <rorywalsh@ear.ie>
2019-09-07 11:07:41 +0100

    updating Windows CI build

--- buildWin.ps1
+++ buildWin.ps1
@@ -1,9 +1,9 @@
 dir  
-# cd c:/
-# Invoke-WebRequest -Uri "https://github.com/rorywalsh/cabbage/releases/download/v2.0.00/csound-windows_x64-6.13.0.zip" -OutFile "C:\csound-windows_x64-6.12.0.zip" 
-# 7z.exe x csound-windows_x64-6.12.0.zip -o"C:/Program Files"
-# cd "C:/Program Files/Csound6_x64"
-# dir
+cd c:/
+Invoke-WebRequest -Uri "https://github.com/rorywalsh/cabbage/releases/download/v2.0.00/csound-windows_x64-6.13.0.zip" -OutFile "C:\csound-windows_x64-6.12.0.zip" 
+7z.exe x csound-windows_x64-6.12.0.zip -o"C:/Program Files"
+cd "C:/Program Files/Csound6_x64"
+dir
 choco install -y msys2 --params "/InstallDir:C:\tools\msys64"
 Invoke-WebRequest -Uri "https://vcvrack.com/downloads/Rack-SDK-1.1.4.zip" -OutFile "D:/a/Rack-SDK-1.1.4.zip" 
 cd D:/a
